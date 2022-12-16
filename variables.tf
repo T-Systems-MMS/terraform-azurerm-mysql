703d9c3d0f62514ff33204d5dee8f4f4b85d3e00
Hauke Brandt <hauke.brandt@t-systems.com>
2022-12-16 11:01:23 +0100

    Added resources and locals for mysql flexible server

--- variables.tf
+++ variables.tf
@@ -24,9 +24,30 @@ variable "mysql_database" {
   description = "resource definition, default settings are defined within locals and merged with var settings"
 }
 
+variable "mysql_flexible_server" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
+variable "mysql_flexible_server_configuration" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
+variable "mysql_flexible_server_firewall_rule" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
+variable "mysql_flexible_database" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
+
 locals {
   default = {
-    # resource definition
+    # resource definition for mysql server
     mysql_server = {
       name                              = ""
       administrator_login               = null
@@ -67,12 +88,48 @@ locals {
       charset   = "utf8"
       collation = "utf8_unicode_ci"
     }
+
+    # resource definition for mysql flexible server
+    mysql_flexible_server = {
+      name                              = ""
+      zone                              = 1
+      create_mode                       = "Default"
+      source_server_id                  = null
+      replication_role                  = "None"
+      administrator_login               = null
+      administrator_login_password      = null
+      backup_retention_days             = 7
+      delegated_subnet_id               = null
+      geo_redundant_backup_enabled      = false
+      point_in_time_restore_time_in_utc = null
+      private_dns_zone_id               = null
+      high_availability                 = {}
+      maintenance_window                = {}
+      storage                           = {}
+      tags                              = {}
+    }
+    mysql_flexible_server_configuration = {
+      name = ""
+    }
+    mysql_flexible_server_firewall_rule = {
+      name = ""
+    }
+    mysql_flexible_database = {
+      name      = ""
+      charset   = "utf8"
+      collation = "utf8_unicode_ci"
+    }
   }
 
   # compare and merge custom and default values
   mysql_server_values = {
     for mysql_server in keys(var.mysql_server) :
     mysql_server => merge(local.default.mysql_server, var.mysql_server[mysql_server])
+  }
+
+  mysql_flexible_server_values = {
+    for mysql_flexible_server in keys(var.mysql_flexible_server) :
+    mysql_flexible_server => merge(local.default.mysql_flexible_server, var.mysql_flexible_server[mysql_flexible_server])
   }
 
   # merge all custom and default values
@@ -104,5 +161,32 @@ locals {
   mysql_database = {
     for mysql_database in keys(var.mysql_database) :
     mysql_database => merge(local.default.mysql_database, var.mysql_database[mysql_database])
+  }
+
+  mysql_flexible_server = {
+    for mysql_fflexible_server in keys(var.mysql_flexible_server) :
+    mysql_flexible_server => merge(
+      local.mysql_flexible_server_values[mysql_flexible_server],
+      {
+        for config in [
+          "high_availability",
+          "maintenance_window",
+          "storage"
+        ] :
+        config => merge(local.default.mysql_flexible_server[config], local.mysql_flexible_server_values[mysql_flexible_server][config])
+      }
+    )
+  }
+  mysql_flexible_server_configuration = {
+    for mysql_flexible_server_configuration in keys(var.mysql_flexible_server_configuration) :
+    mysql_flexible_server_configuration => merge(local.default.mysql_flexible_server_configuration, var.mysql_flexible_server_configuration[mysql_flexible_server_configuration])
+  }
+  mysql_flexible_server_firewall_rule = {
+    for mysql_flexible_server_firewall_rule in keys(var.mysql_flexible_server_firewall_rule) :
+    mysql_flexible_server_firewall_rule => merge(local.default.mysql_flexible_server_firewall_rule, var.mysql_flexible_server_firewall_rule[mysql_flexible_server_firewall_rule])
+  }
+  mysql_flexible_database = {
+    for mysql_flexible_database in keys(var.mysql_flexible_database) :
+    mysql_flexible_database => merge(local.default.mysql_flexible_database, var.mysql_flexible_database[mysql_flexible_database])
   }
 }
